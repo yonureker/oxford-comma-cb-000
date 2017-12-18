3f84906eac17df08f873b908a2fcf400fb8f96d1
Onur Eker <yonureker@gmail.com>
2017-12-18 22:47:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.first + " and " + array.last
+  elsif array.size > 2
+    array.first + ", " + array[1..(array.size - 2)].join(", ") + ", and " + array.last
+  end
 end
+
+
+#the last element of an array should be added as "and #{element}"
+#all other elements should be added as ", #{element}"
